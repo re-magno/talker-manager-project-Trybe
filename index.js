fd66ceef77a1241e69ba554388aa2709fde54ce3
Renata <renatamagno@live.com>
2022-03-21 18:33:23 -0300

    :sparkles: Create the POST /login endpoint

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { randomUUID } = require('crypto');
 const fs = require('fs').promises;
+const { isValidEmail, isValidPassword } = require('./middlewares/validations');
 
 const app = express();
 app.use(bodyParser.json());
@@ -34,7 +36,6 @@ app.get('/talker/:id', async (req, res, _next) => {
     const talkersParse = JSON.parse(talkers);
 
     const talker = talkersParse.find((t) => t.id === Number(id));
-    console.log(talkersParse);
 
     if (!talker) {
       return res.status(404).json({
@@ -45,6 +46,12 @@ app.get('/talker/:id', async (req, res, _next) => {
   } catch (e) {
     console.log('Erro');
   }
+});
+
+app.post('/login', isValidEmail, isValidPassword, (_req, res) => {
+  const token = randomUUID().substring(0, 16);
+  console.log(token);
+  res.status(200).json({ token });
 });
 
 app.listen(PORT, () => {
