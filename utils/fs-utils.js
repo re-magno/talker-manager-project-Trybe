9477356151a149f5c3835908509dcf80cfe142c3
Renata <renatamagno@live.com>
2022-03-23 18:14:22 -0300

    :sparkles: create function getTakerById(id)

--- utils/fs-utils.js
+++ utils/fs-utils.js
@@ -6,8 +6,14 @@ async function getTakers() {
     return allTalkers;
 }
 
+async function getTakerById(id) {
+  const allTalkers = await getTakers();
+  
+  return allTalkers.find((talker) => talker.id === +id);
+}
+
 async function setTalkers(newTalker) {
   await fs.writeFile('./talker.json', JSON.stringify(newTalker));
 }
 
-module.exports = { getTakers, setTalkers };
+module.exports = { getTakers, setTalkers, getTakerById };
